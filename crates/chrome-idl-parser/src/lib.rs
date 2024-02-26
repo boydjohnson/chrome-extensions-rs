a8267d4192c92548a9ba60ad21f402f86104652a
Boyd Johnson <johnson.boyd@gmail.com>
2024-02-26 17:48:55 -0600

    Allow for multiple from directories

--- crates/chrome-idl-parser/src/lib.rs
+++ crates/chrome-idl-parser/src/lib.rs
@@ -1,7 +1,11 @@
 use {
     crate::json::ChromeApi,
     quote::quote,
-    std::{fs::read_dir, io::Write, path::Path},
+    std::{
+        fs::read_dir,
+        io::Write,
+        path::{Path, PathBuf},
+    },
 };
 
 pub mod json;
@@ -11,58 +15,59 @@ trait ToWasmBindgen {
     fn to_wasm_bindgen(&self) -> proc_macro2::TokenStream;
 }
 
-pub fn generate(from: &Path, to: &Path) -> Result<(), Box<dyn std::error::Error>> {
+pub fn generate(froms: &[PathBuf], to: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let mut mods = vec![];
-
-    for item in read_dir(from)? {
-        let entry = item?;
-        if entry
-            .file_name()
-            .into_string()
-            .unwrap()
-            .ends_with("private.json")
-            || entry.file_name().into_string().unwrap().starts_with('_')
-        {
-            continue;
-        }
-
-        if let Ok(file_type) = entry.file_type() {
-            if !file_type.is_file() {
+    for from in froms {
+        for item in read_dir(from)? {
+            let entry = item?;
+            if entry
+                .file_name()
+                .into_string()
+                .unwrap()
+                .ends_with("private.json")
+                || entry.file_name().into_string().unwrap().starts_with('_')
+            {
                 continue;
             }
-        }
 
-        if !entry.file_name().into_string().unwrap().ends_with(".json") {
-            continue;
-        }
-
-        let buf = std::fs::read_to_string(&entry.path())?;
-
-        println!("{:?}", entry.path());
-
-        let buf = buf
-            .split('\n')
-            .filter(|el| !el.starts_with("//"))
-            .collect::<Vec<_>>()
-            .join("\n");
-
-        match deser_hjson::from_str::<Vec<ChromeApi>>(&buf) {
-            Ok(v) => {
-                let mut to_path = to.to_path_buf();
-                to_path.push(entry.file_name());
-
-                to_path.set_extension("rs");
-
-                mods.push(to_path.file_stem().unwrap().to_str().unwrap().to_string());
-
-                for chrome in v {
-                    let mut output = std::fs::File::create(&to_path)?;
-
-                    output.write_all(format!("{}", chrome.to_wasm_bindgen()).as_bytes())?;
+            if let Ok(file_type) = entry.file_type() {
+                if !file_type.is_file() {
+                    continue;
                 }
             }
-            Err(e) => {
-                println!("{:?}: {:?}", e, entry.path());
+
+            if !entry.file_name().into_string().unwrap().ends_with(".json") {
+                continue;
+            }
+
+            let buf = std::fs::read_to_string(&entry.path())?;
+
+            println!("{:?}", entry.path());
+
+            let buf = buf
+                .split('\n')
+                .filter(|el| !el.starts_with("//"))
+                .collect::<Vec<_>>()
+                .join("\n");
+
+            match deser_hjson::from_str::<Vec<ChromeApi>>(&buf) {
+                Ok(v) => {
+                    let mut to_path = to.to_path_buf();
+                    to_path.push(entry.file_name());
+
+                    to_path.set_extension("rs");
+
+                    mods.push(to_path.file_stem().unwrap().to_str().unwrap().to_string());
+
+                    for chrome in v {
+                        let mut output = std::fs::File::create(&to_path)?;
+
+                        output.write_all(format!("{}", chrome.to_wasm_bindgen()).as_bytes())?;
+                    }
+                }
+                Err(e) => {
+                    println!("{:?}: {:?}", e, entry.path());
+                }
             }
         }
     }

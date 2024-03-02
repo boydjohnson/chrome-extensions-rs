ad53df1949c980a2f68c8a22b539e69f8b6e4550
Boyd Johnson <johnson.boyd@gmail.com>
2024-03-02 16:19:41 -0600

    Test windows.getCurrent

--- js_tests/windows.test.ts
+++ js_tests/windows.test.ts
@@ -1,4 +1,4 @@
-import { windows_get_all } from "../pkg/chrome_extensions";
+import { windows_get_all, windows_get_current } from "../pkg/chrome_extensions";
 
 describe("windows.getAll", () => {
   beforeEach(() => {
@@ -58,5 +58,63 @@ describe("windows.getAll", () => {
         type: "popup",
       },
     ]);
+  });
+});
+
+describe("windows.GetCurrent", () => {
+  it("Should return a window", async () => {
+    jest
+      .spyOn(chrome.windows, "getCurrent")
+      .mockImplementation((queryOptions: chrome.windows.QueryOptions) =>
+        Promise.resolve({
+          id: 123,
+          focused: true,
+          alwaysOnTop: false,
+          incognito: false,
+        }),
+      );
+
+    const windows = await windows_get_current(null);
+
+    expect(windows).toEqual({
+      id: 123,
+      focused: true,
+      alwaysOnTop: false,
+      incognito: false,
+    });
+  });
+
+  it("Should return only some windows with queryOptions", async () => {
+    jest
+      .spyOn(chrome.windows, "getCurrent")
+      .mockImplementation((queryOptions: chrome.windows.QueryOptions) =>
+        Promise.resolve(
+          queryOptions.windowTypes && queryOptions.windowTypes.includes("popup")
+            ? {
+                id: 456,
+                focused: false,
+                alwaysOnTop: true,
+                incognito: true,
+                type: "popup",
+              }
+            : {
+                id: 123,
+                focused: true,
+                alwaysOnTop: false,
+                incognito: false,
+                type: "normal",
+              },
+        ),
+      );
+
+    const queryOptions = { windowTypes: ["popup"] };
+    const windows = await windows_get_current(queryOptions);
+    expect(windows).toEqual({
+      id: 456,
+      focused: false,
+      alwaysOnTop: true,
+      incognito: true,
+      type: "popup",
+    });
   });
 });
